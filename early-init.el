cd05a19206947bc117b0f9252f5e5aa55751eec9
Johan Sjöberg <johan@letrec.se>
2022-12-04 19:46:45 +0100

    keep it simple

--- early-init.el
+++ early-init.el
@@ -1,23 +1,23 @@
-(setq
- gc-cons-threshold most-positive-fixnum  ; motto: clean later
- large-file-warning-threshold (* 100 1024 1024)
- inhibit-splash-screen t
- ;;
- backup-directory-alist `(("." . "~/.emacs.d/backup/"))
- backup-by-copying t
- delete-old-versions t
- kept-new-versions 6
- kept-old-versions 2
- version-control t
- ;;
- c-default-style "linux"
- c-basic-offset 4
- tab-width 4
- indent-tabs-mode nil
- ;;
- ido-enable-flex-matching t
- ido-case-fold t
- )
+;; vanilla section
+
+(setq gc-cons-threshold (* 32 1024 1024))
+(setq large-file-warning-threshold (* 100 1024 1024))
+(setq inhibit-splash-screen t)
+
+(setq backup-directory-alist `(("." . "~/.emacs.d/backup/")))
+(setq backup-by-copying t)
+(setq delete-old-versions t)
+(setq kept-new-versions 6)
+(setq kept-old-versions 2)
+(setq version-control t)
+
+(setq c-default-style "linux")
+(setq c-basic-offset 4)
+(setq tab-width 4)
+(setq indent-tabs-mode nil)
+
+(setq ido-enable-flex-matching t)
+(setq ido-case-fold t)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
@@ -25,31 +25,34 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(scroll-bar-lines . 0) default-frame-alist)
 
-(defvar big-face-state 't)
-(defun big-face ()
-  (interactive)
-  (setq big-face-state (not big-face-state))
-  (set-face-attribute
-   'default nil
-   :family (if big-face-state "Spleen 32x64" "Spleen 16x32")
-   :height (if big-face-state 250 100)))
 (mapc
  (lambda (face)
    (set-face-attribute face nil :weight 'normal :underline nil))
  (face-list))
-(set-face-attribute 'default nil :weight 'normal)
-(big-face)
+
+(defvar big-face-state)
+
+(defun big-face (state)
+  (setq big-face-state state)
+  (set-face-attribute
+   'default nil
+   :family (if state "Spleen 32x64" "Spleen 16x32")
+   :height (if state 250 100)
+   :weight 'normal))
+
+(defun big-face-toggle ()
+  (interactive)
+  (big-face (not big-face-state)))
+
+(big-face nil)
 
 (add-hook 'after-init-hook 'ido-mode)
 (add-hook 'c++-mode-hook 'eglot-ensure)
 (add-hook 'c-mode-hook 'eglot-ensure)
 
-(add-hook
- 'after-init-hook
- (lambda ()
-   (setq gc-cons-threshold (* 32 1024 1024))))  ; motto: keep your mottos
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; end of vanilla section
+;; third party section
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -64,30 +67,23 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(mapc
- 'straight-use-package
- '(use-package
-   company
-   doom-modeline
-   dracula-theme
-   magit
-   meow
-   rustic
-   tree-sitter
-   tree-sitter-langs
-   vterm
-   vterm-toggle))
-
-(setq
- company-idle-delay 0.0
- company-minimum-prefix-length 1
- )
-(add-hook 'after-init-hook 'global-company-mode)
-
-(add-hook 'after-init-hook 'doom-modeline-mode)
+(straight-use-package 'use-package)
+(straight-use-package 'company)
+(straight-use-package 'doom-modeline)
+(straight-use-package 'dracula-theme)
+(straight-use-package 'magit)
+(straight-use-package 'meow)
+(straight-use-package 'rustic)
+(straight-use-package 'tree-sitter)
+(straight-use-package 'tree-sitter-langs)
+(straight-use-package 'vterm)
+(straight-use-package 'vterm-toggle)
 
 (load-theme 'dracula t)
-
+(setq company-idle-delay 0.0)
+(setq company-minimum-prefix-length 1)
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'doom-modeline-mode)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
 (defun meow-setup ()
@@ -99,59 +95,101 @@
       (<AC04> "f" "F") (<AC05> "g" "G") (<AC06> "h" "H") (<AC07> "j" "J") (<AC08> "k" "K") (<AC09> "l" "L") (<AC10> "ö" "Ö")
       (<AC11> "ä" "Ä") (<BKSL> "'" "*") (<LSGT> "<" ">") (<AB01> "z" "Z") (<AB02> "x" "X") (<AB03> "c" "C") (<AB04> "v" "V")
       (<AB05> "b" "B") (<AB06> "n" "N") (<AB07> "m" "M") (<AB08> "," ";") (<AB09> "." ":") (<AB10> "-" "_")))
-  (setq
-   meow-cheatsheet-physical-layout meow-cheatsheet-physical-layout-iso
-   meow-cheatsheet-layout meow-cheatsheet-layout-qwerty-swedish)
+  (setq meow-cheatsheet-physical-layout meow-cheatsheet-physical-layout-iso)
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty-swedish)
+  (meow-define-keys 'insert '("C-c" . meow-normal-mode))
   (meow-motion-overwrite-define-key
-   '("j" . meow-next) '("k" . meow-prev) '("<escape>" . ignore))
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
   (meow-leader-define-key
    ;; SPC j/k will run the original command in MOTION state.
-   '("j" . "H-j") '("k" . "H-k")
+   '("j" . "H-j")
+   '("k" . "H-k")
    ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument) '("2" . meow-digit-argument)      '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument) '("5" . meow-digit-argument)      '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument) '("8" . meow-digit-argument)      '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument) '("/" . meow-keypad-describe-key) '("?" . meow-cheatsheet))
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
   (meow-normal-define-key
-   '("0" . meow-expand-0) '("9" . meow-expand-9) '("8" . meow-expand-8)
-   '("7" . meow-expand-7) '("6" . meow-expand-6) '("5" . meow-expand-5)
-   '("4" . meow-expand-4) '("3" . meow-expand-3) '("2" . meow-expand-2)
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
    '("1" . meow-expand-1)
-   '("-" . negative-argument)   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing) '("." . meow-bounds-of-thing)
-   '("a" . meow-append)    '("A" . meow-open-below)
-   '("b" . meow-back-word) '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)    '("D" . meow-backward-delete)
-   '("e" . meow-next-word) '("E" . meow-next-symbol)
-   '("f" . meow-find)      '("F" . meow-goto-line)
-   '("g" . meow-cancel-selection) '("G" . meow-grab)
-   '("h" . meow-left)      '("H" . meow-left-expand)
-   '("i" . meow-insert)    '("I" . meow-open-above)
-   '("j" . meow-next)      '("J" . meow-next-expand)
-   '("k" . meow-prev)      '("K" . meow-prev-expand)
-   '("l" . meow-right)     '("L" . meow-right-expand)
-   '("m" . meow-join)      '("M" . magit)
-   '("n" . meow-search)    '("N" . display-line-numbers-mode)
-   '("o" . meow-block)     '("O" . meow-to-block)
-   '("p" . meow-yank)      '("P" . previous-buffer)
-   '("q" . meow-quit)      '("Q" . meow-goto-line)
-   '("r" . meow-replace)   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
-   '("t" . meow-till)      '("T" . vterm-toggle)
-   '("u" . meow-undo)      '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)     '("V" . big-face)
-   '("w" . meow-mark-word) '("W" . meow-mark-symbol)
-   '("x" . meow-line)      '("X" . meow-goto-line)
-   '("y" . meow-save)      '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   ;'("å" . nil)
-   '("ä" . meow-end-of-thing)       '("Ä" . eglot-find-declaration)
-   '("ö" . meow-beginning-of-thing) '("Ö" . eglot-find-implementation)
-   '("'" . repeat)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
    '("§" . ido-find-file)
-   '("<escape>" . ignore))
-  (meow-define-keys 'insert '("C-c" . meow-normal-mode)))
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("M" . magit)
+   '("n" . meow-search)
+   '("N" . display-line-numbers-mode)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("P" . previous-buffer)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("T" . vterm-toggle)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("V" . big-face-toggle)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("å" . ido-find-file)
+   '("ä" . meow-end-of-thing)
+   '("Ä" . eglot-find-declaration)
+   '("ö" . meow-beginning-of-thing)
+   '("Ö" . eglot-find-implementation)
+   '("'" . repeat)
+   '("<escape>" . ignore)))
 
 (require 'meow)
 (meow-setup)
